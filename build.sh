0797ea856c0057df35e00f2b992b27e0f398c15b
JZJisawesome <40674968+JZJisawesome@users.noreply.github.com>
2018-06-30 10:48:55 -0400

    add a script to build 15Slide

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir ./obj
+mkdir ./obj/Grid15
+g++ -Wall -fexceptions -std=c++1z -Og -g -Isrc -Iinclude -c ./src/CommandUI.cpp -o ./obj/CommandUI.o
+g++ -Wall -fexceptions -std=c++1z -Og -g -Isrc -Iinclude -c ./src/Grid15/Grid.cpp -o ./obj/Grid15/Grid.o
+g++ -Wall -fexceptions -std=c++1z -Og -g -Isrc -Iinclude -c ./src/Grid15/GridHelp.cpp -o ./obj/Grid15/GridHelp.o
+g++ -Wall -fexceptions -std=c++1z -Og -g -Isrc -Iinclude -c ./src/main.cpp -o ./obj/main.o
+
+mkdir ./bin
+g++ -Lsrc -Linclude -o ./bin/15Slide ./obj/CommandUI.o ./obj/Grid15/Grid.o ./obj/Grid15/GridHelp.o ./obj/main.o
+
+echo "Done compiling 15Slide"
