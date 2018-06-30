3fd3cae120a3c94be1b8af95895a766deae7d4c2
JZJisawesome <40674968+JZJisawesome@users.noreply.github.com>
2018-06-30 08:51:27 -0400

    Revert "Revert "merging stuff to GTKSlide like fixed grid solving and author name""

--- src/Grid15/GridManager.h
+++ src/Grid15/GridManager.h
@@ -12,6 +12,7 @@ namespace Grid15
  *
  * \brief Manages storage, initialization, movement of tiles, and saving/loading of a grid. DONT USE: IT'S DECPRECATED
  *
+ * \author John Jekel 
  * \date 2017-2018
  *
  * \deprecated Use Grid and GridHelp instead
