bd26cb4f4182b1f47526a5f3a2464f1cda1b1f3e
JZJisawesome <40674968+JZJisawesome@users.noreply.github.com>
2018-06-30 09:37:49 -0400

    Things are comming along, and the grid problem is kind of fixed; upping version

--- src/ProgramStuff.h
+++ src/ProgramStuff.h
@@ -121,7 +121,7 @@ constexpr bool CLEAR_SCREEN_ON_START {false};///<Clear Screen on start with acsi
 
 namespace Build
 {
-constexpr float SLIDE_VERSION   {0.5};      ///<15Slide Version
+constexpr float SLIDE_VERSION   {0.6};      ///<15Slide Version
 
 constexpr bool DEBUG            {true};     ///<Debug build
 constexpr bool RELEASE          {!DEBUG};   ///<Release build
